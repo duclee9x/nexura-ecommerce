c82aa7202706cb2cdfe70addc8400009ae157798
Duc Lee <ledangduc7601@gmail.com>
2025-06-17 03:49:58 +0700

    fix: change permission start.sh

--- a.sh
+++
@@ -1,5 +0,0 @@
-services=$(git diff --name-only origin/main...HEAD | grep '^src/' | cut -d '/' -f2 | uniq | cut -d '-' -f1 | tr '\n' ',' | sed 's/,$//')
-if [ -z "$services" ]; then
-    services="user,product,payment,order,cart,frontend,workflow"
-fi
-echo "services=$services"
